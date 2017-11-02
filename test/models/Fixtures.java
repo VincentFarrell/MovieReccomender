4e563c178785214ce09ea77369623ea63dbde1c8
Chentay <Chentay@Ruskie-PeeCee>
2017-11-02 16:07:55 +0000

    Further Updated TestCases

--- test/models/Fixtures.java
+++ test/models/Fixtures.java
@@ -5,5 +5,23 @@ public class Fixtures {
 			new User("marge", "simpson", "female", "unemployed", 35),
 			new User("lisa", "simpson", "female", "unemployed", 11),
 			new User("bart", "simpson", "male", "unemployed", 10),
-			new User("maggie", "simpson", "female", "unemployed", 3) };
+			new User("maggie", "simpson", "female", "unemployed", 3)
+	};
+
+	public static Activity[] activities =
+	{
+	  new Activity ("walk",  "fridge", 0.001),
+	  new Activity ("walk",  "bar",    1.0),
+	  new Activity ("run",   "work",   2.2),
+	  new Activity ("walk",  "shop",   2.5),
+	  new Activity ("cycle", "school", 4.5)
+	};
+	
+	public static Location[]locations =
+	{
+	  new Location(23.3f, 33.3f),
+	  new Location(34.4f, 45.2f),  
+	  new Location(25.3f, 34.3f),
+	  new Location(44.4f, 23.3f)       
+	};
 }
--- test/models/UserTest.java
+++ test/models/UserTest.java
@@ -4,39 +4,38 @@ import static org.junit.Assert.*;
 
 import java.util.HashSet;
 import java.util.Set;
-
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-public class UserTest {
-	private User[] users = { 
-			new User("marge", "simpson", "female", "unemployed", 5),
-			new User("lisa", "simpson", "female", "unemployed", 10),
-			new User("bart", "simpson", "male", "unemployed", 11),
-			new User("maggie", "simpson", "female", "unemployed", 3) };
-	User homer = new User("homer", "simpson", "male", "unemployed", 40);
+import static models.Fixtures.users;
 
-	@Test
-	public void testCreate() {
-		assertEquals("homer", homer.firstName);
-		assertEquals("simpson", homer.lastName);
-		assertEquals("male", homer.gender);
-		assertEquals("unemployed", homer.occupation);
-		assertEquals(40, homer.age);
-	}
+public class UserTest
+{
+  User homer = new User ("homer", "simpson", "male",  "unemployed", 40);
 
-	@Test
-	public void testIds() {
-		Set<Long> ids = new HashSet<>();
-		for (User user : users) {
-			ids.add(user.id);
-		}
-		assertEquals(users.length, ids.size());
-	}
+  @Test
+  public void testCreate()
+  {
+    assertEquals ("homer",               homer.firstName);
+    assertEquals ("simpson",             homer.lastName);
+    assertEquals ("male",   			 homer.gender);   
+    assertEquals ("unemployed",          homer.occupation);  
+    assertEquals (40,              		 homer.age); 
+  }
 
-	@Test
-	public void testToString() {
-		assertEquals("User{" + homer.id + ", homer, simpson, male, unemployed, 40}", homer.toString());
-	}
+  @Test
+  public void testIds()
+  {
+    Set<Long> ids = new HashSet<>();
+    for (User user : users)
+    {
+      ids.add(user.id);
+    }
+    assertEquals (users.length, ids.size());
+  }
+
+  @Test
+  public void testToString()
+  {
+    assertEquals ("User{" + homer.id + ", homer, simpson, male, unemployed, 40}", homer.toString());
+  }
 }
