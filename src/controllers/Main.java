bcb1ae2c22769cfa32a247663043577b5515715b
Chentay <Chentay@Ruskie-PeeCee>
2017-11-03 02:55:59 +0000

    Implemented Serializer

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,26 +1,37 @@
 package controllers;
 
-import java.io.IOException;
+import java.io.File;
 import java.util.Collection;
 
 import models.User;
+import utils.Serializer;
+import utils.XMLSerializer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		MovieReccomenderAPI moviereccomenderAPI = new MovieReccomenderAPI();
+	public static void main(String[] args) throws Exception {
 
-		moviereccomenderAPI.createUser("Bart", "Simpson", "Male", "Unemployed", 10);
-		moviereccomenderAPI.createUser("Homer", "Simpson", "Male", "Unemployed", 40);
-		moviereccomenderAPI.createUser("Lisa", "Simpson", "Female", "Unemployed", 11);
+		File datastore = new File("datastore3.xml");
+		Serializer serializer = new XMLSerializer(datastore);
 
-		Collection<User> users = moviereccomenderAPI.getUsers();
+		MovieReccomenderAPI movieReccomenderAPI = new MovieReccomenderAPI(serializer);
+		if (datastore.isFile()) {
+			movieReccomenderAPI.load();
+		}
+
+		movieReccomenderAPI.createUser("Bart", "Simpson", "Male", "Unemployed", 10);
+		movieReccomenderAPI.createUser("Homer", "Simpson", "Male", "Unemployed", 40);
+		movieReccomenderAPI.createUser("Lisa", "Simpson", "Female", "Unemployed", 11);
+
+		Collection<User> users = movieReccomenderAPI.getUsers();
 		System.out.println(users);
 
-		User homer = moviereccomenderAPI.getUserByName("Homer");
-		System.out.println(homer);
+		   User homer = movieReccomenderAPI.getUserByName("Homer");
+		   movieReccomenderAPI.createRating(homer.id, 0L, 1L, 8.5);
 
-		moviereccomenderAPI.deleteUser(homer.id);
-		users = moviereccomenderAPI.getUsers();
+		movieReccomenderAPI.deleteUser(homer.id);
+		users = movieReccomenderAPI.getUsers();
 		System.out.println(users);
+
+		movieReccomenderAPI.store();
 	}
 }
